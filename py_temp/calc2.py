9cdbb12b3bc68c2384cc69da01e7471c30371e8f
양경민 <113006287+gm010516@users.noreply.github.com>
2023-01-11 14:51:13 +0900

    Add files via upload

---
+++ py_temp/calc2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# calc1.py 모듈 파일 저장
+def add(x, y): 
+    return x+y
+
+def sub(x, y): 
+    return x-y
+
+def mul(x, y): 
+    return x*y
+
+def div(x, y): 
+    if y != 0:
+        return x/y
+
